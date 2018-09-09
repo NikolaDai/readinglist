bccdcb7fe9d2d583987b298b706daa20f45ee2ed
Daniel Dai <335860828@qq.com>
2018-09-10 00:39:32 +0800

    fix some bugs and still have more work to do.

--- src/main/java/com/manning/readinglist/ReaderRepository.java
+++ src/main/java/com/manning/readinglist/ReaderRepository.java
@@ -1,6 +1,9 @@
 package com.manning.readinglist;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 
 public interface ReaderRepository extends JpaRepository<Reader, String> {
--- src/main/java/com/manning/readinglist/ReadinglistApplication.java
+++ src/main/java/com/manning/readinglist/ReadinglistApplication.java
@@ -8,15 +8,32 @@ package com.manning.readinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /*enable component-scanning and auto-configuration
 * Before spring boot 1.2.0, you have to use three annotations: @Configuration (designates this class as a configuration class)
 * ,@ComponentScan and @EnableAutoConfiguration instead of @SpringBootApplication.
 * */
 @SpringBootApplication
-public class ReadinglistApplication {
+public class ReadinglistApplication  extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ReadinglistApplication.class, args);
+    }
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new ReaderHandlerMethodArgumentResolver());
     }
 }
